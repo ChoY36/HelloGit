68996ae8ecb1550db045e28f82c5629c1e4a032b
ChoY36 <cycrystalline@gmail.com>
2021-11-16 12:19:09 +0900

    Toast2

--- app/src/main/java/com/example/hellogit/MainActivity.java
+++ app/src/main/java/com/example/hellogit/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "Hello GitHub", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello GitHub2", Toast.LENGTH_SHORT).show();
     }
 }
